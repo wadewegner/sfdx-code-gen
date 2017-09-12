cc5c977e85c31ec90dd6d921375a943f4696920f
Wade Wegner <wade.wegner@gmail.com>
2017-09-12 15:58:07 +0200

    Added support for .sfdx-templates

--- commands/code_create.js
+++ commands/code_create.js
@@ -1,6 +1,9 @@
 const path = require('path');
-const fs = require('fs');
 const fse = require('fs-extra');
+const fs = require('fs');
+const os = require('os');
+
+const codeCreate = require('../lib/code_create.js');
 
 (function () {
   'use strict';
@@ -27,7 +30,7 @@ const fse = require('fs-extra');
       char: 'd',
       description: 'folder for saving the created files',
       hasValue: true,
-      required: true
+      required: false
     }, {
       name: 'vars',
       char: 'v',
@@ -42,77 +45,19 @@ const fse = require('fs-extra');
       const outputdir = context.flags.outputdir;
       const vars = context.flags.vars;
 
-      const templateFolder = path.join('templates', template);
-
+      let templateFolder = path.join(os.homedir(), '.sfdx-templates', template);
       if (!fse.existsSync(templateFolder)) {
-        console.error('ERROR:', `specified template '${template}' doesn't exist`);
-        process.exit(1);
+        templateFolder = path.join(__dirname, '../templates', template);
       }
 
-      const defJsonPath = path.join(templateFolder, 'def.json');
+      codeCreate.createFiles(templateFolder, name, template, vars, outputdir, (err, success) => {
 
-      if (!fse.existsSync(defJsonPath)) {
-        console.error('ERROR:', 'def.json not found');
-        process.exit(1);
-      }
-
-      const defJson = JSON.parse(fs.readFileSync(defJsonPath).toString());
-      const defJsonVars = defJson.vars;
-      const defJsonBundle = defJson.bundle;
-      const defJsonFiles = defJson.files;
-
-      if (defJsonBundle) {
-        console.error('ERROR:', 'Bundles not yet supported');
-        process.exit(1);
-      }
-
-      if (!vars) {
-        console.error('ERROR:', `The following variables are required: ${defJsonVars}`);
-        console.error('ERROR:', 'Specify them like: -v className=myclass,apiName=40.0');
-        process.exit(1);
-      }
-
-      defJsonFiles.forEach((row) => {
-        const fileName = row[0];
-        const fileExtension = row[1];
-
-        if (fileName !== 'def.json') {
-
-          const templateFilePath = path.join(templateFolder, fileName);
-          let content = fs.readFileSync(templateFilePath).toString();
-
-          if (vars.includes(',')) {
-            const splitVars = vars.split(',');
-            splitVars.forEach((value) => {
-              const splitVarValues = value.split('=');
-
-              const varName = splitVarValues[0];
-              const varValue = splitVarValues[1];
-
-              content = content.replace(new RegExp(`{{${varName}}}`, 'g'), varValue);
-            });
-          } else {
-            const splitVarValues = vars.split('=');
-
-            const varName = splitVarValues[0];
-            const varValue = splitVarValues[1];
-
-            content = content.replace(new RegExp(`{{${varName}}}`, 'g'), varValue);
-          }
-
-          let newFile = path.join(outputdir, `${name}.${fileExtension}`);
-          const newFilePath = path.dirname(newFile);
-
-          fse.ensureDirSync(newFilePath);
-
-          fs.writeFile(newFile, content, (err) => {
-            if (err) {
-              return console.log(err);
-            }
-
-            console.log(`Your file was created: ${newFile}`);
-          });
+        if (err) {
+          console.error('ERROR:', err);
+          process.exit(1);
         }
+
+        console.log(success);
       });
     }
   };
